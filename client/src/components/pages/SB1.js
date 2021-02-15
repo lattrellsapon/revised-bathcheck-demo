25f860b3c4be7286779d1e552fbf1404f4973722
Lattrell Sapon <lattrellsapon@gmail.com>
2021-02-16 11:15:51 +1300

    Removed index number for testing

--- client/src/components/pages/SB1.js
+++ client/src/components/pages/SB1.js
@@ -5,10 +5,8 @@ import { SB1_formSwitcher } from '../sb1/SB1_formSwitcher';
 import { SB1_openingStatements } from '../sb1/SB1_openingStatements';
 
 export const SB1 = () => {
-  const { step } = useContext(SB1Context);
   return (
     <div>
-      <h1>{step}</h1>
       <SB1_openingStatements />
       <SB1_formSwitcher />
       <div>
