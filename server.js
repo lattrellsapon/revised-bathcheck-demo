79cbd1b6c83151514f677ef16701ffec974474a2
Lattrell Sapon <lattrellsapon@gmail.com>
2021-02-15 22:19:15 +1300

    Added code to check whether app is in production

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const colors = require('colors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello world');
+// });
+
+if (process.env.NODE_ENV === 'production') {
+  // Set Statoc Folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
