3f8f92b738b4b3f489eae0d72f0b9f807b1dfd42
Lattrell Sapon <lattrellsapon@gmail.com>
2021-02-16 11:05:56 +1300

    Added steps index for testing on the sb1 page

--- client/src/components/pages/SB1.js
+++ client/src/components/pages/SB1.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SB1Context } from '../../context/sb1/SB1State';
 import { SB1_formContainer } from '../sb1/SB1_formContainer';
 import { SB1_formSwitcher } from '../sb1/SB1_formSwitcher';
 import { SB1_openingStatements } from '../sb1/SB1_openingStatements';
 
 export const SB1 = () => {
+  const { step } = useContext(SB1Context);
   return (
     <div>
+      <h1>{step}</h1>
       <SB1_openingStatements />
       <SB1_formSwitcher />
       <div>
